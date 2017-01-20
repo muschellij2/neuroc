d7b682d64706a1cef2316d7870f4949879a78ead
muschellij2 <muschellij2@gmail.com>
2017-01-20 14:42:17 -0500

    updating installer index.html

--- install/index.R
+++ install/index.R
@@ -3,19 +3,19 @@ knitr::opts_chunk$set(echo = TRUE)
 
 ## ---- eval = FALSE-------------------------------------------------------
 ## ## try https:// if http:// URLs are supported
-## source("http://neuroconductor.org/neurocLite.R")
+## source("https://neuroconductor.org/neurocLite.R")
 ## neuro_install("PACKAGE")
 
 ## ----eval = FALSE--------------------------------------------------------
-## source("http://neuroconductor.org/neurocLite.R")
+## source("https://neuroconductor.org/neurocLite.R")
 ## neuro_install(c("fslr", "hcp"))
 
 ## ----eval = FALSE--------------------------------------------------------
-## source("http://neuroconductor.org/neurocLite.R")
+## source("https://neuroconductor.org/neurocLite.R")
 ## neurocLite(c("fslr", "hcp"))
 
 ## ----eval = FALSE--------------------------------------------------------
-## source("http://neuroconductor.org/neurocLite.R")
+## source("https://neuroconductor.org/neurocLite.R")
 ## neuro_install("neurocInstall")
 
 ## ---- eval = FALSE-------------------------------------------------------
