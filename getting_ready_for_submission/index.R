d91f1211a7fd0456464fbe40f4db4d9802da57a9
muschellij2 <muschellij2@gmail.com>
2017-09-12 23:04:42 -0400

    added usethis

--- getting_ready_for_submission/index.R
+++ getting_ready_for_submission/index.R
@@ -2,8 +2,8 @@
 knitr::opts_chunk$set(eval = FALSE)
 
 ## ---- eval = TRUE, echo = FALSE------------------------------------------
-# pkg_name = "usethis"
-pkg_name = "devtools"
+pkg_name = "usethis"
+# pkg_name = "devtools"
 
 ## ---- eval = FALSE-------------------------------------------------------
 ## devtools::build_vignettes()
